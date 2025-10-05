a119e29bf4537295d6dbbb08a2df85966b61a22c
Artem351 <Artem351@users.noreply.github.com>
2025-10-05 10:10:01 +0000

    Deploying to gh-pages from @ Artem351/OOP@8f2d6ab2b2a039ec8248b1ad7e68310d67ede97a 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.pisarev","l":"Card"},{"p":"ru.nsu.pisarev","l":"Cards"},{"p":"ru.nsu.pisarev","l":"Dealer"},{"p":"ru.nsu.pisarev","l":"Player"},{"p":"ru.nsu.pisarev","l":"Runner"}];updateSearchResults();
