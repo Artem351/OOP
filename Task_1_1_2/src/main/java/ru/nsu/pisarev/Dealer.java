4c55892e98f13e60632b029a057b30a97f570369
Artem <191974021+Artem351@users.noreply.github.com>
2025-10-08 09:57:10 +0700

    Update Dealer.java R

--- Task_1_1_2/src/main/java/ru/nsu/pisarev/Dealer.java
+++ Task_1_1_2/src/main/java/ru/nsu/pisarev/Dealer.java
@@ -1,40 +1,29 @@
 package ru.nsu.pisarev;
 
 public class Dealer {
-    public Card[] dealerCards;
+    public Card[] cards;
 
-    public Dealer(Card[] dealerCards) {
-        this.dealerCards = dealerCards;
-    }
-
-    public void setDealerCards(Card[] dealerCards) {
-        this.dealerCards = dealerCards;
+    public Dealer(Card[] cards) {
+        this.cards = cards;
     }
 
-    public void showCardsBeforeOpen(){
-        System.out.print("Карты дилера:");
-        dealerCards[0].showCard();
-        System.out.println("<закрытая карта>");
+    public void setCards(Card[] cards) {
+        this.cards = cards;
     }
-    public void showFirstCardOpen(){
-        System.out.print("Ход дилера\n--------\n");
-        System.out.print("Дилер открывает закрытую карту:");
-        dealerCards[1].showCard();
-        System.out.println();
-    }
-    public void showDealerCardsAfterOpen(){
-        System.out.print("Карты дилера:");
-        dealerCards[0].showCards(dealerCards);
-        System.out.println("=> "+dealerCards[0].sumCards(dealerCards));
 
-    }
+
     public boolean needPickCards(){
         int decision;
-        decision = dealerCards[0].sumCards(dealerCards);
-        if (decision<=16){
-            return true;
-        }
-        return false;
+        Cards.changeAceValue(cards);
+        decision = Card.sumCards(cards);
+        return decision<=16;
     }
 
+    public void changeAceValue() {
+        Card.changeAceValue(cards);
+    }
+
+    public int sumCards() {
+        return Card.sumCards(cards);
+    }
 }
