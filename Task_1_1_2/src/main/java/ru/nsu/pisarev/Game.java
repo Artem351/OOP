ec20f1cfb824266cac7460eb046216633f6a6e63
Artem <191974021+Artem351@users.noreply.github.com>
2025-10-08 09:58:08 +0700

    Update Game.java R

--- Task_1_1_2/src/main/java/ru/nsu/pisarev/Game.java
+++ Task_1_1_2/src/main/java/ru/nsu/pisarev/Game.java
@@ -7,13 +7,49 @@ public class Game {
     final static String[] NAMES_ARRAY = {"Двойка","Тройка","Четвёрка","Пятёрка","Шестёрка","Семёрка","Восьмёрка","Девятка","Десятка",
             "Валет","Дама","Король","Туз"};
 
-    private static final int MAXIMAL_CARDS = 10;
+    private static final int MAXIMAL_CARDS = 20;
 
     public static Player createPlayer() {
         Card[] cards = new Card[MAXIMAL_CARDS];
         cards[0] = createCard();
         cards[1] = createCard();
         return new Player(cards);
+    }
+
+    public static Dealer createDealer() {
+        Card[] cards = new Card[MAXIMAL_CARDS];
+        cards[0] = createCard();
+        cards[1] = createCard();
+        Dealer dealer = new Dealer(cards);
+        boolean decision = false;
+        decision = dealer.needPickCards();
+        while(decision) {
+            if (decision){
+                Card dnewcard = createCard();
+                int i=-1;
+                for (Card dealerCard : dealer.cards) {
+                    i++;
+                    if (dealerCard==null)
+                        break;
+                }
+                dealer.cards[i]=dnewcard;
+                System.out.println();
+            }
+            decision = dealer.needPickCards();
+        }
+        return dealer;
+    }
+    public static Card AddCardPlayer(Player player){
+        Card pcardn = Game.createCard();
+        int i=-1;
+        for (Card card : player.cards) {
+            i++;
+            if (card ==null)
+                break;
+        }
+        player.cards[i]=pcardn;
+        player.changeAceValue();
+        return pcardn;
     }
 
     private static Card createCard(){
