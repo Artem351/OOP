99a956b3811c33e1f3cbe72a29edfe11876f06b2
Artem351 <191974021+Artem351@users.noreply.github.com>
2025-11-09 21:00:34 +0700

    Fix

--- Task_1_2_1/src/main/java/ru/nsu/pisarev/AdjacencyListGraph.java
+++ Task_1_2_1/src/main/java/ru/nsu/pisarev/AdjacencyListGraph.java
@@ -1,9 +1,6 @@
 package ru.nsu.pisarev;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- Task_1_2_1/src/main/java/ru/nsu/pisarev/Graph.java
+++ Task_1_2_1/src/main/java/ru/nsu/pisarev/Graph.java
@@ -1,7 +1,6 @@
 package ru.nsu.pisarev;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+
 
 /**
  * Represents a general graph structure that contains vertices and edges.
