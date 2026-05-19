4dc9c838a44ffbb675dfd5d68a0b5282bb5120b1
Artem351 <Artem351@users.noreply.github.com>
2026-05-19 03:40:42 +0000

    Deploying to gh-pages from @ Artem351/OOP@bfa176e57737e7291b2a4bdbdc06ecf5ee2ea399 🚀

--- Task_2_3_1/package-search-index.js
+++ Task_2_3_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"TaskSnake","l":"ru.nsu.pisarev"},{"m":"TaskSnake","l":"ru.nsu.pisarev.model"},{"m":"TaskSnake","l":"ru.nsu.pisarev.view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"TaskSnake","l":"ru.nsu.pisarev"},{"m":"TaskSnake","l":"ru.nsu.pisarev.controller"},{"m":"TaskSnake","l":"ru.nsu.pisarev.model"},{"m":"TaskSnake","l":"ru.nsu.pisarev.view"}];updateSearchResults();
